4efd7ec48e95453e62c247a4c96ffa4b0d81556d
Aren Clissold <arenclissold@gmail.com>
2022-04-20 15:38:51 +1000

    conversion of app to vue3 app

---
+++ vueapp/src/App.vue
@@ -0,0 +1,612 @@
+<template>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-to-top">
+    <div class="container-fluid">
+      <a class="navbar-brand relative" href="#"
+        ><span class="initial">A</span
+        ><svg
+          class="brand"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          viewBox="0 0 100 100"
+          fill="none"
+          stroke="gray"
+          stroke-width="4"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></svg>
+        <title>Aren</title>>
+        <polygon points="50 3,100 28,100 75, 50 100,3 75,3 25"
+      /></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+        <a href="" class="button-link">Resume</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col">
+        <section>
+          <h5>Hi, my name is</h5>
+          <h1>Aren</h1>
+          <h2>I build things for the web</h2>
+          <p id="about">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum sunt
+            fugit consectetur in magnam, dolore quisquam incidunt similique
+            praesentium officia quaerat, nostrum blanditiis rerum corporis
+            eveniet nisi! Voluptates, ex reiciendis. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Dolore, odit. Necessitatibus vitae
+            delectus voluptates pariatur. Nihil numquam, illo explicabo quas
+            possimus molestias eveniet dolorum atque quaerat optio vero, quis
+            ipsam!
+          </p>
+        </section>
+        <section>
+          <div class="sub-heading">
+            <h2>About</h2>
+            <div class="line"></div>
+          </div>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col col">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Totam sunt unde aliquid soluta maiores voluptatem fugiat
+                  excepturi, eveniet, expedita ratione magni culpa optio
+                  adipisci omnis, voluptatum facilis aliquam cum nesciunt.
+                  <br /><br />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ducimus hic, repudiandae ad iusto atque impedit fugiat amet
+                  numquam nihil laboriosam nulla pariatur soluta dignissimos
+                  minus perferendis? Laborum optio quos tempora.
+                </p>
+                <ul class="skill-list">
+                  <li>Ruby</li>
+                  <div class="list-line"></div>
+                  <li>Ruby on Rails</li>
+                  <div class="list-line"></div>
+                  <li>JavaScript</li>
+                  <div class="list-line"></div>
+                  <li>DOM Manipulation</li>
+                  <div class="list-line"></div>
+                  <li>HTML5</li>
+                  <div class="list-line"></div>
+                  <li>CSS3</li>
+                  <div class="list-line"></div>
+                  <li>Git</li>
+                  <div class="list-line"></div>
+                  <li>GitHub</li>
+                  <div class="list-line"></div>
+                  <li>MongoDB</li>
+                  <div class="list-line"></div>
+                  <li>Node</li>
+                  <div class="list-line"></div>
+                  <li>Express</li>
+                  <div class="list-line"></div>
+                  <li>AJAX</li>
+                  <div class="list-line"></div>
+                  <li>REST APIs</li>
+                </ul>
+              </div>
+              <div class="col">
+                <div class="flex-center">
+                  <div id="projects" class="photo-parent">
+                    <img src="assets/test_pic.png" alt="profile picture" id="photo" />
+                    <div class="photo-border"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="sub-heading">
+            <h2>Things I've Built</h2>
+            <div class="line"></div>
+          </div>
+          <div class="project-cards">
+            <div class="project-card">
+              <div class="project-opacity"></div>
+              <img
+                class="project-img"
+                src=""
+                alt="camping"
+              />
+              <div class="project-body">
+                <h3>Camping App</h3>
+                <p class="project-description">
+                  A modern styled application to find, share and review camping
+                  spots. To find that perfect camp spot and miss the bad ones.
+                </p>
+                <p class="project-technologies">
+                  Express| MongoDB | mongoose | Node | REST
+                </p>
+                <div class="project-links">
+                  <a href=""
+                    ><svg
+                      class="project-git"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="gray"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></svg>
+                    <title>GitHub</title>
+                    <path
+                      d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                    >
+                    </path
+                  ></a>
+                  <a href="" class="project-link"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="project-link"
+                    >
+                      <title>External Link</title>
+                      <path
+                        d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                      ></path>
+                      <polyline points="15 3 21 3 21 9"></polyline>
+                      <line x1="10" y1="14" x2="21" y2="3"></line></svg
+                  ></a>
+                </div>
+              </div>
+            </div>
+            <div class="project-card">
+              <div class="project-opacity"></div>
+              <img
+                class="project-img"
+                src=""
+                alt="camping"
+              />
+              <div class="project-body">
+                <h3>Camping App</h3>
+                <p>
+                  A modern styled application to find, share and review camping
+                  spots. To find that perfect camp spot and miss the bad ones.
+                </p>
+                <p class="project-technologies">
+                  Express| MongoDB | mongoose | Node | REST
+                </p>
+                <div class="project-links">
+                  <a href=""
+                    ><svg
+                      class="project-git"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="gray"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <title>GitHub</title>
+                      <path
+                        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                      ></path></svg
+                  ></a>
+                  <a href="" class="project-link"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="project-link"
+                    >
+                      <title>External Link</title>
+                      <path
+                        d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                      ></path>
+                      <polyline points="15 3 21 3 21 9"></polyline>
+                      <line x1="10" y1="14" x2="21" y2="3"></line></svg
+                  ></a>
+                </div>
+              </div>
+            </div>
+            <div class="project-card">
+              <div class="project-opacity"></div>
+              <img
+                class="project-img"
+                src=""
+                alt="camping"
+              />
+              <div class="project-body">
+                <h3>Camping App</h3>
+                <p>
+                  A modern styled application to find, share and review camping
+                  spots. To find that perfect camp spot and miss the bad ones.
+                </p>
+                <p class="project-technologies">
+                  Express| MongoDB | mongoose | Node | REST
+                </p>
+                <div class="project-links">
+                  <a href=""
+                    ><svg
+                      class="project-git"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="gray"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <title>GitHub</title>
+                      <path
+                        d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                      ></path></svg
+                  ></a>
+                  <a href="" class="project-link"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="project-link"
+                    >
+                      <title>External Link</title>
+                      <path
+                        d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                      ></path>
+                      <polyline points="15 3 21 3 21 9"></polyline>
+                      <line x1="10" y1="14" x2="21" y2="3"></line></svg
+                  ></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="centered margin-bottom">
+          <div class="sub-heading center-content">
+            <div class="small-line"></div>
+            <h2 id="contact">Get in Touch</h2>
+            <div class="small-line"></div>
+          </div>
+          <p class="last">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            corrupti laboriosam architecto incidunt aliquid reprehenderit
+            facilis, aliquam illum voluptatibus suscipit eum dolorem rerum
+            ratione odio assumenda beatae laudantium impedit nostrum.
+          </p>
+          <a href="mailto:arenclissold@gmail.com" class="hello-button"
+            >Say Hello</a
+          >
+        </section>
+        <footer>
+          <div class="footer-links">
+            <a href=""
+              ><svg
+                class="project-git"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="gray"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>GitHub</title>
+                <path
+                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                ></path></svg
+            ></a>
+            <a href=""
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="linkedin"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>LinkedIn</title>
+                <path
+                  d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                ></path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle></svg
+            ></a>
+            <a href="mailto:arenclissold@gmail.com"
+              ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                <path
+                  d="M0 128C0 92.65 28.65 64 64 64H448C483.3 64 512 92.65 512 128V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V128zM48 128V150.1L220.5 291.7C241.1 308.7 270.9 308.7 291.5 291.7L464 150.1V127.1C464 119.2 456.8 111.1 448 111.1H64C55.16 111.1 48 119.2 48 127.1L48 128zM48 212.2V384C48 392.8 55.16 400 64 400H448C456.8 400 464 392.8 464 384V212.2L322 328.8C283.6 360.3 228.4 360.3 189.1 328.8L48 212.2z"
+                />
+              </svg>
+            </a>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #0a1128;
+  color: #aeb9df;
+  font-family: "Roboto", sans-serif;
+}
+
+svg:hover {
+  stroke: #4cc9f0;
+}
+
+.centered {
+  text-align: center;
+}
+
+.center-content {
+  justify-content: center;
+}
+
+.relative {
+  position: relative;
+}
+
+.fixed-to-top {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(4px);
+  z-index: 10;
+}
+
+.brand {
+  stroke: #4cc9f0;
+  height: 50px;
+  width: 75px;
+}
+
+.initial {
+  position: absolute;
+  left: 29px;
+  top: 12px;
+  font-size: 25px;
+  color: #4cc9f0;
+}
+
+section {
+  margin-top: 7em;
+}
+
+.button-link {
+  border: solid #4cc9f0;
+  border-radius: 5px;
+  padding: 0.5em 1em;
+  transition: 0.5s;
+}
+
+.button-link:hover {
+  color: #0a1128;
+  background-color: #4cc9f0;
+}
+
+a {
+  text-decoration: none;
+  color: #4cc9f0;
+}
+
+.line {
+  height: 1px;
+  width: 50%;
+  background: #2f3957;
+  margin: 0em 2em;
+}
+
+.small-line {
+  height: 1px;
+  width: 20%;
+  background: #2f3957;
+  margin: 0em 2em;
+}
+
+.sub-heading {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 0.5em;
+}
+.skill-list {
+  margin-top: 2em;
+  width: 80%;
+}
+
+.skill-list > li {
+  display: inline;
+}
+
+.list-line {
+  display: inline-block;
+  width: 3px;
+  height: 3px;
+  background-color: white;
+  margin: 0px 0.5em 0.2em 0.5em;
+  transform: rotate(45deg);
+}
+
+.photo-parent {
+  position: relative;
+}
+
+.photo-border {
+  border: 0.2em solid #4cc9f0;
+  border-radius: 0.4em;
+  top: 1.5em;
+  left: 1.5em;
+  height: 20em;
+  width: 20em;
+  position: absolute;
+  z-index: 0;
+  transition: 0.5s;
+}
+
+#photo {
+  height: 20em;
+  width: 20em;
+  border-radius: 0.2em;
+  z-index: 1;
+  position: relative;
+}
+
+#photo:hover ~ .photo-border {
+  top: 1em;
+  left: 1em;
+}
+
+.flex-center {
+  display: flex;
+  justify-content: center;
+  margin: 2em;
+}
+
+.project-cards {
+  display: flex;
+  flex-direction: column;
+}
+
+.project-card {
+  position: relative;
+  height: 20em;
+  width: auto;
+  box-shadow: 0px 8px 24px #00032c4f;
+  margin: 2em 0em;
+}
+
+.project-body {
+  z-index: 2;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  box-shadow: inset darkblue;
+  padding: 1em;
+  width: 100%;
+  height: 100%;
+}
+
+.project-img {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 0;
+  border-radius: 5px;
+}
+
+.project-opacity {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: 1;
+  background-color: #001f54;
+  opacity: 0.8;
+  border-radius: 5px;
+}
+
+.project-git {
+  width: 2em;
+  height: 2em;
+  stroke: #aeb9df;
+  margin-right: 0.5em;
+  transition: 0.5s;
+}
+
+.project-link {
+  width: 2em;
+  height: 2em;
+  stroke: #aeb9df;
+  transition: 0.5s;
+}
+
+.last-p {
+  width: 60%;
+}
+
+.hello-button {
+  border: solid #4cc9f0;
+  display: inline-block;
+  border-radius: 5px;
+  padding: 1rem 2rem;
+  transition: 0.5s;
+}
+
+.hello-button:hover {
+  color: #0a1128;
+  background-color: #4cc9f0;
+}
+
+.linkedin {
+  width: 2em;
+  height: 2em;
+  stroke: #aeb9df;
+  transition: 0.5s;
+}
+
+.footer-links {
+  display: flex;
+  justify-content: center;
+  margin: 2em 0em;
+  gap: 1em;
+}
+
+.mail {
+  stroke: #aeb9df;
+}
+</style>
